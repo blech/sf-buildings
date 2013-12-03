9523c078cce38f10ddbb1ce85060701123011859
Paul Mison <paul@bynd.com>
2013-12-03 15:46:51 -0800

    Update shell convert and python split scripts

--- convert.sh
+++ convert.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
+
 wget http://apps.sfgov.org/datafiles/view.php?file=sfgis/building_footprint.zip -o building_footprint.zip
 unzip building_footprint.zip
 ogr2ogr -s_srs EPSG:2872 -t_srs EPSG:4326 bf_ll.shp building_footprint.shp
-ogrinfo -al -so bf_ll.shp | grep 'Extent'
-ogr2ogr -spat -122.409840333 37.7897243333 -122.383735667 37.8108446667 -f GeoJSON bf_ll-4_4.json bf_ll.shp
-node_modules/.bin/topojson bf_ll-4_4.json > bf_ll-4_4.topojson
+python split.py
 
-# a full list of splits can be built from ogr2ogr -spat -122.409840333 37.7897243333 -122.383735667 37.8108446667 -f GeoJSON bf_ll-4_4.json bf_ll.shp
--- split.py
+++ split.py
@@ -2,6 +2,7 @@
 # splits a shapefile into a 5x5 grid of TopoJSON files
 
 # TODO pull extents automatically using ogrinfo -al -so
+#      ogrinfo -al -so bf_ll.shp | grep 'Extent'
 # TODO configure number of boxes in grid
 # CRAZY adaptive splits based on polygon count (NB I am not that clever)
 
@@ -13,20 +14,23 @@ tr = (-122.357631, 37.831965)
 xinc = (tr[0]-bl[0])/6
 yinc = (tr[1]-bl[1])/6
 
-for y in range(0,5):
-    for x in range(0,5):
+for x in range(0,6):
+    for y in range(0,6):
 
         x1 = bl[0]+x*xinc
         x2 = bl[0]+(x+1)*xinc
 
-        y1 = bl[1]+y*yinc
-        y2 = bl[1]+(y+1)*yinc
+        y1 = tr[1]-y*yinc
+        y2 = tr[1]-(y+1)*yinc
 
-        ogr2ogr = "/usr/bin/ogr2ogr -spat %s %s %s %s -f GeoJSON bf_ll-%s_%s.geojson bf_ll.shp" % (x1, y1, x2, y2, x, y)
-        topojson = "/usr/bin/topojson bf_ll-%s_%s.geojson -o bf_ll-%s_%s.topojson" % (x, y, x, y)
+        ogr2ogr = "/usr/bin/ogr2ogr -spat %s %s %s %s -f GeoJSON bf_ll-%s_%s.geojson bf_ll.shp" % (x1, y2, x2, y1, x, y)
+        topojson = "/usr/bin/topojson bf_ll-%s_%s.geojson -o topojson/bf_ll-%s_%s.topojson" % (x, y, x, y)
+
+        print ogr2ogr
 
         subprocess.call(ogr2ogr, shell=True)
         subprocess.call(topojson, shell=True)
+        subprocess.call("rm bf_ll-%s_%s.geojson" % (x, y), shell=True)
 
         print "Completed block %s %s" % (x, y)
 
