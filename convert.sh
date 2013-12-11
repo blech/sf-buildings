6bd57e8e19e8e308937b39080610d6661f74bec9
Paul Mison <misonp+git@gmail.com>
2013-12-10 23:40:59 -0800

    Remove non-HTML files from gh-pages branch

--- convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget http://apps.sfgov.org/datafiles/view.php?file=sfgis/building_footprint.zip -o building_footprint.zip
-unzip building_footprint.zip
-ogr2ogr -s_srs EPSG:2872 -t_srs EPSG:4326 bf_ll.shp building_footprint.shp
-python split.py
-
